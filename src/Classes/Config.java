fac13b3a6d2a5b126976f9079f5639fff9bd1a61
AndresDRomeroA <68675951+AndresDRomeroA@users.noreply.github.com>
2023-10-13 00:30:18 -0400

    Acomodades los archivos

---
+++ src/Classes/Config.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+
+public class Config {
+    
+    //General
+    private int dayDuration;
+    private int deliveryDays;
+    //Lamborghini
+    private int initialChasis;
+    private int initialBodys;
+    private int initialWheels;
+    private int initialMotors;
+    private int initialAccesorys;
+    private int initialAssemblers;
+    //Maserati
+    private int initialChasisM;
+    private int initialBodysM;
+    private int initialWheelsM;
+    private int initialMotorsM;
+    private int initialAccesorysM;
+    private int initialAssemblersM;
+
+    public Config() {
+        this.dayDuration = 1000;
+        this.deliveryDays = 30;
+        this.initialChasis = 1;
+        this.initialWheels = 1;
+        this.initialMotors = 1;
+        this.initialAccesorys = 1;
+        this.initialAssemblers = 1;
+        this.initialChasisM = 1;
+        this.initialWheelsM = 1;
+        this.initialMotorsM = 1;
+        this.initialAccesorysM = 1;
+        this.initialAssemblersM = 1;
+    }
+
+ 
+    
+    //Getters and Setters
+    public int getDayDuration() {
+        return dayDuration;
+    }
+
+    public void setDayDuration(int dayDuration) {
+        this.dayDuration = dayDuration;
+    }
+
+    public int getDeliveryDays() {
+        return deliveryDays;
+    }
+
+    public void setDeliveryDays(int deliveryDays) {
+        this.deliveryDays = deliveryDays;
+    }
+
+    public int getInitialChasis() {
+        return initialChasis;
+    }
+
+    public void setInitialChasis(int initialChasis) {
+        this.initialChasis = initialChasis;
+    }
+
+    public int getInitialWheels() {
+        return initialWheels;
+    }
+
+    public void setInitialWheels(int initialWheels) {
+        this.initialWheels = initialWheels;
+    }
+
+    public int getInitialMotors() {
+        return initialMotors;
+    }
+
+    public void setInitialMotors(int initialMotors) {
+        this.initialMotors = initialMotors;
+    }
+
+    public int getInitialAccesorys() {
+        return initialAccesorys;
+    }
+
+    public void setInitialAccesorys(int initialAccesorys) {
+        this.initialAccesorys = initialAccesorys;
+    }
+
+    public int getInitialAssemblers() {
+        return initialAssemblers;
+    }
+
+    public void setInitialAssemblers(int initialAssemblers) {
+        this.initialAssemblers = initialAssemblers;
+    }
+
+    public int getInitialChasisM() {
+        return initialChasisM;
+    }
+
+    public void setInitialChasisM(int initialChasisM) {
+        this.initialChasisM = initialChasisM;
+    }
+
+    public int getInitialWheelsM() {
+        return initialWheelsM;
+    }
+
+    public void setInitialWheelsM(int initialWheelsM) {
+        this.initialWheelsM = initialWheelsM;
+    }
+
+    public int getInitialMotorsM() {
+        return initialMotorsM;
+    }
+
+    public void setInitialMotorsM(int initialMotorsM) {
+        this.initialMotorsM = initialMotorsM;
+    }
+
+    public int getInitialAccesorysM() {
+        return initialAccesorysM;
+    }
+
+    public void setInitialAccesorysM(int initialAccesorysM) {
+        this.initialAccesorysM = initialAccesorysM;
+    }
+
+    public int getInitialAssemblersM() {
+        return initialAssemblersM;
+    }
+
+    public void setInitialAssemblersM(int initialAssemblersM) {
+        this.initialAssemblersM = initialAssemblersM;
+    }
+
+    public int getInitialBodys() {
+        return initialBodys;
+    }
+
+    public void setInitialBodys(int initialBodys) {
+        this.initialBodys = initialBodys;
+    }
+
+    public int getInitialBodysM() {
+        return initialBodysM;
+    }
+
+    public void setInitialBodysM(int initialBodysM) {
+        this.initialBodysM = initialBodysM;
+    }
+    
+    
+    
+}
